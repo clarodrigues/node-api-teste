998053a45eadd43e7168b862e5b75cb79e6fd5ed
Claudio Rodrigues <claudio.rodrigues@adcos.com.br>
2020-02-02 16:22:57 -0300

    Criando a conexão com o mongodb, criando a model product e instalando o require-dir

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const requireDir = require("require-dir");
 
-app.get("/", (req, res) => {
-    res.send("Hello Cláudio");
+//instanciando o orm mongoose e realizando a conexão
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:17017/nodeapi", { useNewUrlParser: true });
+
+//Require dir importando o diretorio dos models
+requireDir("./src/models");
+
+//Instanciando um Product
+const Product = mongoose.model("Product");
+
+app.get("/", (req, res) => {    
+    return res.send("Hello Cláudio");
 });
 
 app.listen(3000);
